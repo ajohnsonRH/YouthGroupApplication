cea01ea6bca74162fc33dbd1399d1ac27adb77a0
Adalyn Johnson <johnsoaa@rose-hulman.edu>
2015-07-19 13:09:00 -0400

    added project to github

---
+++ ChurchApplication/app/src/androidTest/java/church_application/rose_hulman/edu/churchapplication/ApplicationTest.java
@@ -0,0 +1,13 @@
+package church_application.rose_hulman.edu.churchapplication;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ ChurchApplication/app/src/main/java/church_application/rose_hulman/edu/churchapplication/HomeActivity.java
@@ -0,0 +1,39 @@
+package church_application.rose_hulman.edu.churchapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.*;
+import android.widget.ImageButton;
+
+public class HomeActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        ((ImageButton) findViewById(R.id.aboutUsButton)).setOnClickListener(this);
+        ((ImageButton) findViewById(R.id.tenbytenButton)).setOnClickListener(this);
+        ((ImageButton) findViewById(R.id.announcementsButton)).setOnClickListener(this);
+        ((ImageButton) findViewById(R.id.eventsButton)).setOnClickListener(this);
+        Log.e("READY FOR TAKE OFF", "Button Listeners have been set");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.aboutUsButton:
+                Log.e("Button Click", "Launch AboutUsActivity");
+                break;
+            case R.id.tenbytenButton:
+                Log.e("Button Click", "Launch TenByTenActivity");
+                break;
+            case R.id.announcementsButton:
+                Log.e("Button Click", "Launch AnnouncementsActivity");
+                break;
+            case R.id.eventsButton:
+                Log.e("Button Click", "Launch EventsActivity");
+                break;
+        }
+    }
+}
