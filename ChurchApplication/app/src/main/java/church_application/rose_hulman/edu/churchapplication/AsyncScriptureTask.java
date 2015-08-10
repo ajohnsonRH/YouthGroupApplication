fbe727a46183fc720f4d41444c782694d5b7fbca
Adalyn Johnson <johnsoaa@rose-hulman.edu>
2015-08-10 15:08:05 -0400

    -Back buttons stopped working, fixed back button functionality -Added functionality to connect to ESV API

---
+++ ChurchApplication/app/src/main/java/church_application/rose_hulman/edu/churchapplication/AsyncScriptureTask.java
@@ -0,0 +1,57 @@
+package church_application.rose_hulman.edu.churchapplication;
+
+import android.os.AsyncTask;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by johnsoaa on 8/10/2015.
+ */
+public class AsyncScriptureTask extends AsyncTask<Void, Void, Void> {
+    //private String httpget = "http://www.esvapi.org/v2/rest/passageQuery?key=IP&passage=";
+    private String testhttpget = "http://www.esvapi.org/v2/rest/passageQuery?key=IP&passage=John+1";
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        URL url;
+        HttpURLConnection urlConnection = null;
+        try {
+            url = new URL(testhttpget);
+
+            urlConnection = (HttpURLConnection) url
+                    .openConnection();
+            InputStream in = urlConnection.getInputStream();
+
+            InputStreamReader isw = new InputStreamReader(in);
+
+            int data = isw.read();
+            while (data != -1) {
+                char current = (char) data;
+                data = isw.read();
+                System.out.print(current);
+            }
+            urlConnection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                urlConnection.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace(); //If you want further info on failure...
+            }
+        }
+        return null;
+    }
+
+    protected void onPreExecute() {
+        //display progress dialog.
+
+    }
+
+    protected void onPostExecute(Void result) {
+        // dismiss progress dialog and update ui
+    }
+}
--- ChurchApplication/app/src/main/java/church_application/rose_hulman/edu/churchapplication/TenByTenActivity.java
+++ ChurchApplication/app/src/main/java/church_application/rose_hulman/edu/churchapplication/TenByTenActivity.java
@@ -9,17 +9,23 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class TenByTenActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ten_by_ten);
         getActionBar().setDisplayHomeAsUpEnabled(true);
-        TextView textView = (TextView)findViewById(R.id.daily_scripture_text);
+        TextView textView = (TextView) findViewById(R.id.daily_scripture_text);
         textView.setText("Scripture text/daily reading/verse references");
+        new AsyncScriptureTask().execute();
     }
+
     @Override
     public void onBackPressed() {
         // super.onBackPressed();
@@ -30,8 +36,7 @@ public class TenByTenActivity extends Activity {
 
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem menuItem)
-    {
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
         onBackPressed();
         return true;
     }
